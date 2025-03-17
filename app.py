6077ed88b3a0484f47b2ef494148e11dd20f2508
vjvaidya06 <vjvaidya2006@gmail.com>
2025-03-17 19:36:19 -0400

    Update app.py
    Added couple comments for future reference

--- app.py
+++ app.py
@@ -2,10 +2,20 @@ import json
 from jsonstream import load
 from generate_response import *
 #make more interactive later on, for now just pull all the prompts from prompt.json and generate responses
-# GR = Gen_Resp()
 f = open("prompts.json", "r")
 stream = load(f)
+#Keeps track of whether we're generating or analyzing, maybe make interactive later.
+mode = 'generate'
+# mode = 'analyze'
+#Keeps track of all policies
+#In the future check it with a file that keeps track of prompts we've already generated responses for
 policies = []
-for p in stream:
-    policies.append(p)
+#Generate some number of responses for each LLM and save as JSON files
+
+if mode == 'generate':
+    #How many iterations we generate for
+    iterations = 5
+    for p in stream:
+        policies.append(p)
+
     
