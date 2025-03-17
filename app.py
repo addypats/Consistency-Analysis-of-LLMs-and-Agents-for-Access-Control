3311b8f84e25bb9990c00187202cd814ade52360
addypats <adityapat1999@gmail.com>
2025-03-17 16:53:26 -0400

    Added, deleted files. Added functionalities

--- app.py
+++ app.py
@@ -13,13 +13,15 @@ from spider_charts import Spider_Charts
 from cross_valid_list import Cross_Valid_List
 
 
-R = Result()
-RA = Result_Answers()
-LMP = Llm_Model_Process()
-SL = Spider_List()
-API = api()
-SPC = Spider_Charts()
-CVL = Cross_Valid_List()
+# Class initializations from other python files
+
+# R = Result()
+# RA = Result_Answers()
+# LMP = Llm_Model_Process()
+# SL = Spider_List()
+# API = api()
+# SPC = Spider_Charts()
+# CVL = Cross_Valid_List()
 
 
 app = Flask(__name__)
@@ -36,8 +38,43 @@ def home():
         return render_template('index.html')
     
     
-    # Template for the actual Falsk code
+    # Template for the actual Flask code
+
+@app.route('/upload_file_modified', methods=['GET', 'POST'])
+def upload_file_modified():
+
+    # initializations
  
+    if request.method == 'POST':
+
+        # get parameters from user
+        file = request.files['file']
+        # counter = request.form.get('counter')     # Not needed for now
+        # topic = request.form.get('topic')
+        
+        # read the file and get the descriptions
+        lines = file.stream.readlines()
+        questions_list = [line.decode('utf-8').strip() for line in lines]
+
+
+        # print("DATA_DICT_ANSWERS")
+        # print(data_dict_answers)
+
+        # f = open("log.txt", 'w')
+        # print(data_dict_questions, file=f)
+        # f.write("\n\n\n\n\n\n\n\n\n\n\n\n")
+        # print(data_dict_answers, file=f)
+        # f.close()
+        
+        # no_of_questions = len(questions_list)
+
+        
+        # return render_template('results.html', questions_list=questions_list)
+        return None
+            
+    else:
+        return render_template('upload_file_modified.html') 
+
     
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port=8000, debug = True)
