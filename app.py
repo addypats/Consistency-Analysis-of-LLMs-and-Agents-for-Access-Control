82668a92356401eac4febdc80171f858a582c683
vjvaidya06 <vjvaidya2006@gmail.com>
2025-03-17 23:43:10 -0400

    Added LLM prompting
    Added LLM prompting. Will save responses to files for fault tolerance. Also further cleaned up generate_response.py.

--- app.py
+++ app.py
@@ -1,21 +1,59 @@
 import json
 from jsonstream import load
 from generate_response import *
-#make more interactive later on, for now just pull all the prompts from prompt.json and generate responses
-f = open("prompts.json", "r")
-stream = load(f)
+import os
 #Keeps track of whether we're generating or analyzing, maybe make interactive later.
 mode = 'generate'
 # mode = 'analyze'
 #Keeps track of all policies
-#In the future check it with a file that keeps track of prompts we've already generated responses for
-policies = []
 #Generate some number of responses for each LLM and save as JSON files
 
 if mode == 'generate':
+    #make more interactive later on, for now just pull all the prompts from prompt.json and generate responses
+    #In the future check it with a file that keeps track of prompts we've already generated responses for
+    policies = []
+    f = open("prompts.json", "r")
+    stream = load(f)
     #How many iterations we generate for
     iterations = 5
+    overwrite = False
+    cnt = 0
+    dirs = [name for name in os.listdir("./Responses") if os.path.isdir(os.path.join("./Responses", name))]
     for p in stream:
         policies.append(p)
+        prompt = "Generate me a /etc/security/pwquality.conf file with the following password policy rules (reply with just the file):\n" + p
+        for d in dirs:
+            #idk what to call the folders, so for now I'll just number them and store the prompt in a txt file inside
+            if not os.path.exists(f"./Responses/{d}/{cnt}/prompt.txt"):
+                os.makedirs(os.path.dirname(f"./Responses/{d}/{cnt}/"), exist_ok=True)
+                f = open(f"./Responses/{d}/{cnt}/prompt.txt", "w")
+                f.write(prompt)
+        #Generate and save
+        #Using files instead of storing it all in memory will help with fault tolerance
+        for i in range(iterations):
+            for d in dirs:
+                #Fault tolerance
+                if os.path.exists(f"./Responses/{d}/{cnt}/pwquality{i}.conf"):
+                    continue
+                resp = ""
+                #Don't have a nicer looking way of doing this rn
+                if d == 'Bloom':
+                    resp = generate_response_bloom(prompt)
+                elif d == 'Cohere':
+                    resp = generate_response_Cohere(prompt)
+                #In progress
+                # elif d == 'DeepSeek':
+                #     resp = generate_response_Cohere(prompt)
+                elif d == 'Gemini':
+                    resp = generate_response_gemini(prompt)
+                elif d == 'GPT 4o-Mini':
+                    resp = generate_response_chat4om(prompt)
+                elif d == 'GPT o3-Mini':
+                    resp = generate_response_chato3m(prompt)
+                elif d == 'Llama3':
+                    resp = generate_response_Llama(prompt)
+                f = open(f"./Responses/{d}/{cnt}/pwquality{i}.conf", "w")
+                f.write(resp)
 
-    
+            
+        cnt+=1
--- generate_response.py
+++ generate_response.py
@@ -2,116 +2,145 @@
 
 import openai
 import os
-import requests
-import json
-import cohere
-import json
 import torch
 import google.generativeai as genai
-from llamaapi import LlamaAPI
 from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer, OPTForQuestionAnswering
 from dotenv import load_dotenv
-import sys
-import time
+import cohere
+from llamaapi import LlamaAPI
+import ollama
+
 #ChatGPT 4o Mini
-def generate_response_chato3m(self, prompt):
-    self.prompt = prompt
+def generate_response_chato3m(prompt):
+    openai.api_key = os.getenv("Chat_GPT_API_Key")
 
-    openai_api_key_personal = os.getenv("Chat_GPT_API_Key")
-
-    openai.api_key = openai_api_key_personal
-
-    self.model_engine = "o3-mini"
-
-    self.prompt = prompt.strip()
-    self.completions = openai.ChatCompletion.create(
-        model=self.model_engine,
+    model_engine = "o3-mini"
+    completions = openai.ChatCompletion.create(
+        model=model_engine,
         messages=[
         {"role": "system", "content": ""},
-        {"role": "user", "content": self.prompt},
+        {"role": "user", "content": prompt.strip()},
     ],
         max_tokens=50,
         n=1,
         stop=None,
         temperature=0.5,
     )
-    self.message = self.completions.choices[0].message.content.strip()
+    message = completions.choices[0].message.content.strip()
 
 
-    return self.message
+    return message
 
 #Meta OPT
-def generate_response_meta(self, prompt):
-    self.prompt = prompt
+# def generate_response_meta(self, prompt):
+#     self.prompt = prompt
     
-    torch.manual_seed(4)
+#     torch.manual_seed(4)
 
-    self.tokenizer = AutoTokenizer.from_pretrained("facebook/opt-350m")
-    self.model = AutoModelForCausalLM.from_pretrained("facebook/opt-350m")
+#     self.tokenizer = AutoTokenizer.from_pretrained("facebook/opt-350m")
+#     self.model = AutoModelForCausalLM.from_pretrained("facebook/opt-350m")
     
-    self.inputs = self.tokenizer(self.prompt, return_tensors="pt")
+#     self.inputs = self.tokenizer(self.prompt, return_tensors="pt")
     
-    with torch.no_grad():
-        self.outputs = self.model.generate(
-            input_ids=self.inputs.input_ids,
-            num_return_sequences=1,
-            max_new_tokens=30,
-            pad_token_id=self.tokenizer.eos_token_id,
+#     with torch.no_grad():
+#         self.outputs = self.model.generate(
+#             input_ids=self.inputs.input_ids,
+#             num_return_sequences=1,
+#             max_new_tokens=30,
+#             pad_token_id=self.tokenizer.eos_token_id,
 
-        )
+#         )
 
-    self.predicted = self.tokenizer.decode(self.outputs[0], skip_special_tokens=True)
-    return self.predicted[(len(self.prompt) + 1):]
+#     self.predicted = self.tokenizer.decode(self.outputs[0], skip_special_tokens=True)
+#     return self.predicted[(len(self.prompt) + 1):]
+# def generate_response_deepseek(self, prompt):
+#     messages = [{"role": "user", "content": "Who are you?"},]
+#     pipe = pipeline("text-generation", model="deepseek-ai/DeepSeek-V3", trust_remote_code=True)
+#     pipe(messages)
 
 #Bloom
-def generate_response_bloom(self, prompt):
+def generate_response_bloom(prompt):
+    checkpoint = "bigscience/bloomz-1b7"
 
-    self.prompt = prompt
-    self.checkpoint = "bigscience/bloomz-1b7"
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+    model = AutoModelForCausalLM.from_pretrained(checkpoint)
 
-    self.tokenizer = AutoTokenizer.from_pretrained(self.checkpoint)
-    self.model = AutoModelForCausalLM.from_pretrained(self.checkpoint)
+    input = tokenizer.encode(prompt, return_tensors="pt")
 
-    self.input = self.tokenizer.encode(self.prompt, return_tensors="pt")
+    outputs = model.generate(input, max_length=2500)
 
-    self.outputs = self.model.generate(self.input, max_length=2500)
+    message = tokenizer.decode(outputs[0])
 
-    self.message = self.tokenizer.decode(self.outputs[0])
-
-    return self.message[(len(self.prompt) + 1):]
+    return message[(len(prompt) + 1):]
 
 #Gemini
-def generate_response_gemini(self, prompt):
+def generate_response_gemini(prompt):
         
-    self.prompt = prompt
+    prompt = prompt
     genai.configure(api_key=os.getenv("Google_Gemini_API_Key"))
     model = genai.GenerativeModel('gemini-1.5-flash')
-    response = model.generate_content(self.prompt)
-    self.message = response.text
+    response = model.generate_content(prompt)
+    message = response.text
     
-    return self.message
+    return message
 
 #ChatGPT 4o Mini
-def generate_response_chat4om(self, prompt):
-    self.prompt = prompt
+def generate_response_chat4om(prompt):
+    openai.api_key = os.getenv("Chat_GPT_API_Key")
 
-    openai_api_key_personal = os.getenv("Chat_GPT_API_Key")
+    model_engine = "gpt-4o-mini"
 
-    openai.api_key = openai_api_key_personal
-
-    self.model_engine = "gpt-4o-mini"
-
-    self.prompt = prompt.strip()
-    self.completions = openai.ChatCompletion.create(
-        model=self.model_engine,
+    completions = openai.ChatCompletion.create(
+        model=model_engine,
         messages=[
         {"role": "system", "content": ""},
-        {"role": "user", "content": self.prompt},
+        {"role": "user", "content": prompt.strip()},
     ],
         max_tokens=50,
         n=1,
         stop=None,
         temperature=0.5,
     )
-    self.message = self.completions.choices[0].message.content.strip()
-    return self.message
+    message = completions.choices[0].message.content.strip()
+    return message
+
+def generate_response_Cohere(prompt):  
+        Api_key = os.getenv("Cohere_API_Key")
+        co = cohere.Client(Api_key)
+        response = co.chat(
+            message=prompt
+        )
+        return response.text.strip()
+
+# def generate_response_Llama(self, prompt):
+    
+#     self.prompt = prompt
+#     # Replace 'Your_API_Token' with your actual API token
+#     self.Api_key = os.getenv("Llama_API_Key")
+#     self.llama = LlamaAPI(self.Api_key)
+    
+#     # API Request JSON Cell
+#     self.api_request_json = {
+#     "model": "llama3-70b",
+#     "messages": [
+#         {"role": "system", "content": ""},
+#         {"role": "user", "content": self.prompt},
+#     ]
+#     }
+
+#     # Make your request and handle the response
+#     self.response = self.llama.run(self.api_request_json)
+#     self.jsonfile = self.response.json()
+#     self.message = self.jsonfile['choices'][0]['message']['content']
+    
+#     return self.message
+#Ran out of tokens, so running Llama3 locally with Ollama
+#Download Ollama and run ollama pull Llama3
+def generate_response_Llama(prompt):
+    response = ollama.chat(model='llama3', messages=[
+        {
+            'role': 'user',
+            'content': prompt,
+        },
+    ])
+    return response['message']['content'].strip()
